ecd9cc72ed32d8fdbd8ceb4d9ddd6a4b430e9f7d
Shannon Rachal <srachal674@gmail.com>
2022-06-21 14:02:39 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 basic.forever(function () {
-	
+    strip.showColor(neopixel.rgb(70, 29, 124))
+    basic.pause(2000)
+    strip.showColor(neopixel.rgb(255, 208, 35))
+    basic.pause(2000)
 })
